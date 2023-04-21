8d0433bbd82d36fe7bf2e85409ff9cf9eb49e059
Heiner Monge <heinermongelopez@gmail.com>
2023-04-20 20:44:08 -0600

    Implemented image carousel on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  images: string[] = [
+    "assets/img/home-slides/slide (1).jpg",
+    "assets/img/home-slides/slide (2).jpg",
+    "assets/img/home-slides/slide (3).jpg",
+    "assets/img/home-slides/slide (4).jpg",
+    "assets/img/home-slides/slide (5).jpg",
+    "assets/img/home-slides/slide (6).jpg",
+    "assets/img/home-slides/slide (7).jpg",
+    "assets/img/home-slides/slide (8).jpg",
+    "assets/img/home-slides/slide (9).jpg",
+    "assets/img/home-slides/slide (10).jpg",
+    "assets/img/home-slides/slide (11).jpg",
+    "assets/img/home-slides/slide (12).jpg",
+    "assets/img/home-slides/slide (13).jpg",
+    "assets/img/home-slides/slide (14).jpg",
+    "assets/img/home-slides/slide (15).jpg",
+    "assets/img/home-slides/slide (16).jpg",
+    "assets/img/home-slides/slide (17).jpg",
+    "assets/img/home-slides/slide (18).jpg"
+  ]
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 3000;
+  }
 }
