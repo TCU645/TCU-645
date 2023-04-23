7e82a8fd7af5d99a1d8b640ecc678c05f04826b1
Heiner Monge <heinermongelopez@gmail.com>
2023-04-23 13:36:46 -0600

    Added basic structure to new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 const routes: Routes = [
-  {
-    path: '**', 
-    component: HomeComponent
-  }
+  { path: 'app-home',component: HomeComponent},
+  { path: 'app-about', component: AboutComponent},
+  { path: 'app-article-list', component: ArticleListComponent},
+  { path: 'app-article', component: ArticleComponent},
+  { path: 'app-resources', component: ResourcesComponent},
+  { path: '**', component: HomeComponent}
 ];
 
 @NgModule({
