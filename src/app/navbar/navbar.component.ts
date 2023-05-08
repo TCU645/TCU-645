6c6a5e15887b337e99f27bffe435a424e9e372d6
Heiner Monge <heinermongelopez@gmail.com>
2023-05-08 16:20:02 -0600

    Added responsive design to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { HandsetData } from 'src/app/models/HandsetData';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  handsetData: HandsetData = HandsetData.getInstance()
+
+  collapsed: boolean = true;
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe(Breakpoints.Web).subscribe((result: any) => {
+      if (result.matches) {
+        this.handsetData.setHandset(false)
+      } else {
+        this.handsetData.setHandset(true)
+      }
+    })
+  }
+
+  public toggle() {
+    this.collapsed = !this.collapsed;
+  }
 }
